ae52659d79d406612cd4de1c161071ba405c8edb
Ajay957 <ajay74.aa82@gmail.com>
2022-11-29 15:26:51 +0530

    update viewphoto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewPhotoComponent } from './view-photo/view-photo.component';
+import { HttpClientModule } from '@angular/common/http';
+
+const myRoute:Routes=[
+  {
+    path:"",
+    component:ViewPhotoComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewPhotoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
